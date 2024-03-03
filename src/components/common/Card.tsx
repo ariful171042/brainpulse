80f645efe0f1d67698a6df5d7f439368673eb813
Sami Khan <sami999khan999@gmail.com>
2024-03-04 00:06:56 +0600

    card added

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -5,21 +5,23 @@ type cardProp = {
   image?: string | undefined;
   title: string;
   description: string;
+  height: number;
+  width: number;
 };
 
-const Card = ({ image, title, description }: cardProp) => {
+const Card = ({ image, title, description, height, width }: cardProp) => {
   return (
-    <div className="p-10 bg-white w-[34rem] h-[34rem] rounded-2xl flex ">
-      <div className="flex flex-col gap-20 items-center">
+    <div className=" bg-black p-[2px] rounded-3xl bg-gradient-to-b from-pink-400 via-purple-500 to-vaiolet">
+      <div className="flex flex-col gap-20 p-10 rounded-[calc(1.5rem-1px)] items-center bg-gradient-to-r from-slate-800 to-slate-700">
         <Image
           src={image || "/assets/img/temp.svg"}
-          height={400}
-          width={400}
+          height={height}
+          width={width}
           alt="cardImage"
         />
-        <div className=" text-black text-left flex flex-col gap-3">
-          <h3 className="text-2xl">{title}</h3>
-          <p className="text-lg">{description}</p>
+        <div className=" text-white/70 text-left flex flex-col gap-3">
+          <h3 className="text-2xl text-white md:text-xl">{title}</h3>
+          <p className="text-lg md:text-sm">{description}</p>
         </div>
       </div>
     </div>
