61e0123d6ac35c6bdbf0a250f438e5faca1d84ba
Sami Khan <sami999khan999@gmail.com>
2024-03-12 16:36:42 +0600

    about init

--- src/components/common/ModileNav.tsx
+++ src/components/common/ModileNav.tsx
@@ -9,33 +9,32 @@ const MobileNav = () => {
 
   return (
     <div>
-      <button onClick={() => setIsOpen((prv) => !prv)}>
+      <button onClick={() => setIsOpen((prev) => !prev)}>
         <div className="text-gray-400 text-2xl text-center pt-2">
           <IoMenu />
         </div>
       </button>
 
-      {isOpen && (
-        <div className="fixed inset-0 z-50 flex justify-end text-gray-300 backdrop-blur-sm">
-          <div
-            className="w-2/5 bg-gray-800/75"
-            onClick={() => setIsOpen((prv) => !prv)}
-          ></div>
+      <div
+        className={`fixed inset-0 z-50 flex justify-end text-gray-300 backdrop-blur-sm transition-transform ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="w-2/5" onClick={() => setIsOpen(false)}></div>
 
-          <div className="w-3/5 flex flex-col gap-5 bg-black/90 p-4 modal-content">
-            <div className="flex justify-between items-center">
-              <UserButton afterSignOutUrl="/" />
-              <div>LOGO</div>
-            </div>
+        <div className="w-3/5 flex flex-col gap-5 bg-black/90 p-4 modal-content">
+          <div className="flex justify-between items-center">
+            <UserButton afterSignOutUrl="/" />
+            <div>LOGO</div>
+          </div>
 
-            <div className="border-b border-gray-500"></div>
+          <div className="border-b border-gray-500"></div>
 
-            <div className="w-full " onClick={() => setIsOpen((prv) => !prv)}>
-              <NavItems />
-            </div>
+          <div className="w-full" onClick={() => setIsOpen(false)}>
+            <NavItems />
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
